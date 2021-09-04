f64d58df4fd8229f16d411e7d4cb32b7700d692d
adrianartale <23aartale@athenian.org>
2021-09-04 13:28:01 -0700

    attempt 1

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,27 +4,44 @@ public class MyMain {
 
     // Calculates the median of the three inputs
     public static int median(int a, int b, int c) {
-        return 0; // REPLACE WITH YOUR CODE
+        if (a>b && a<c){
+            return a;
+        }
+        else if (a<b && a>c){
+            return a;
+        }
+        else if (b>a && b<c){
+            return b;
+        }
+        else if (b<a && b>c){
+            return b;
+        }
+        else
+            return c;
     }
 
     // Returns the input with the larger absolute value
     public static int magnitude(int a, int b) {
-        return 0; // REPLACE WITH YOUR CODE
-    }
+        if (Math.abs(a) > Math.abs(b)) {
+            System.out.println(a);
+        } else
+            System.out.println(b);
 
-    // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
-    // where "a" and "b" are the inputs to the method
-    public static double pythagoras(int a, int b) {
-        return 0.0; // REPLACE WITH YOUR CODE
-    }
+        // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
+        // where "a" and "b" are the inputs to the method
+        public static double pythagoras ( int a, int b){
+            a = (int) Math.pow(a, 2);
+            b = (int) Math.pow(b, 2);
+            double c = (int) Math.sqrt((a + b));
+            return (int) c;
+        }
 
-    public static void main(String[] args) {
-        // You may want to keep these lines of code to test that your methods work
-        System.out.println(median(1, 2, 3)); // should be 2
-        System.out.println(magnitude(-7, -1)); // should be -7
-        System.out.println(pythagoras(3, 4)); // should be 5.0
+        public static void main (String[]args){
+            // You may want to keep these lines of code to test that your methods work
+            System.out.println(median(1, 2, 3)); // should be 2
+            System.out.println(magnitude(-7, -1)); // should be -7
+            System.out.println(pythagoras(3, 4)); // should be 5.0
 
-        Scanner scan = new Scanner(System.in);
-        // YOUR CODE HERE
+            Scanner scan = new Scanner(System.in);
+        }
     }
-}
