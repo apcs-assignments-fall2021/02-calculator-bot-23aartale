4401fb405eef95dec67c98cc5d29eb215e52190b
adrianartale <23aartale@athenian.org>
2021-09-05 13:29:58 -0700

    attempt 2

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -23,9 +23,11 @@ public class MyMain {
     // Returns the input with the larger absolute value
     public static int magnitude(int a, int b) {
         if (Math.abs(a) > Math.abs(b)) {
-            System.out.println(a);
-        } else
-            System.out.println(b);
+            return a;
+        } else {
+            return b;
+        }
+    }
 
         // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
         // where "a" and "b" are the inputs to the method
@@ -37,7 +39,18 @@ public class MyMain {
         }
 
         public static void main (String[]args){
-            // You may want to keep these lines of code to test that your methods work
+            System.out.println("Which math function do you want to use?");
+            System.out.println("1: Median");
+            System.out.println("2: Magnitude");
+            System.out.println("3: Pyhtagorean Theorum");
+            System.out.println("Choose the number that corresponds to your choice");
+            //string choice1 = scan.next();
+            //if choice1 = "1";{
+             //   return median();
+            //}
+
+
+        // You may want to keep these lines of code to test that your methods work
             System.out.println(median(1, 2, 3)); // should be 2
             System.out.println(magnitude(-7, -1)); // should be -7
             System.out.println(pythagoras(3, 4)); // should be 5.0
